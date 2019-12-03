963a3c59c63f98634ef285ebb8bebb7a5fdfd0f3
Hinault <ngdonfack@gmail.com>
2019-12-03 12:51:24 -0500

    Update registerdialog

--- src/ChatBot/Dialogs/RegisterDialog.cs
+++ src/ChatBot/Dialogs/RegisterDialog.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -76,12 +78,19 @@ public RegisterDialog (UserState userState) : base(nameof(RegisterDialog))
 
         private async Task<DialogTurnResult> ProfileStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            stepContext.Values["email"] = (string)stepContext.Result;
+
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Please select your ."),
+                    Choices = ChoiceFactory.ToChoices(new List<string> { "Developer", "Administrator", "Analyst", "Architect" }),
+                }, cancellationToken);
         }
 
         private async Task<DialogTurnResult> AmountPeopleStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["email"] = (string)stepContext.Result;
+            stepContext.Values["profile"] = ((FoundChoice)stepContext.Result).Value;
 
             return await stepContext.PromptAsync(nameof(NumberPrompt<int>), new PromptOptions { Prompt = MessageFactory.Text("Please enter number of attendes (max 3).") }, cancellationToken);
 
@@ -91,17 +100,46 @@ public RegisterDialog (UserState userState) : base(nameof(RegisterDialog))
 
         private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            stepContext.Values["amountpeople"] = (int)stepContext.Result;
+
+            // Get the current registerData object from user state.
+            var registerData = await _registerDataAccessor.GetAsync(stepContext.Context, () => new RegisterData(), cancellationToken);
+
+            registerData.FirstName = (string)stepContext.Values["firstname"];
+            registerData.LastName = (string)stepContext.Values["lastname"];
+            registerData.Email = (string)stepContext.Values["email"];
+            registerData.Profile = (string)stepContext.Values["profile"];
+            registerData.AmountPeople = (int)stepContext.Values["amountpeople"];
+
+            var msg = $"Thanks for your registration. We are reserved {registerData.AmountPeople} seat for you.";
+
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+
+            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is the end.
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
         }
 
         private Task<bool> EmailPromptValidatorAsync(PromptValidatorContext<string> promptContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var result = promptContext.Recognized.Value;
+
+            // This condition is our validation rule.
+            if (new EmailAddressAttribute().IsValid(result))
+            {
+                // Success is indicated by passing back the value the Prompt has collected. You must pass back a value even if you haven't changed it.
+                return Task.FromResult(true);
+            }
+
+            // Not calling End indicates validation failure. This will trigger a RetryPrompt if one has been defined.
+            return Task.FromResult(false);
         }
+
 
         private Task<bool> AmountPeoplePromptValidatorAsync(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // This condition is our validation rule. You can also change the value at this point.
+            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0 && promptContext.Recognized.Value <=3);
+
         }
     }
 
