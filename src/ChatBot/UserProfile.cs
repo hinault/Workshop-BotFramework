a7223a0eea46998ffa515bad456a18dfaa716610
Hinault <ngdonfack@gmail.com>
2019-12-03 09:38:05 -0500

    Add UserProfile

---
+++ src/ChatBot/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    public class UserProfile
+    {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string Email { get; set; }
+
+           public int AmountPeople { get; set; } 
+    }
+}
